924e9da5ef700b55a9ecc8682c687941583cb8d8
JohanTv <johan.tanta@utec.edu.pe>
2024-04-15 21:03:19 -0500

    feat(settings): Add routers for 'modulo0', 'modulo1', 'modulo2', and 'usuarios' modules

--- config/routing.py
+++ config/routing.py
@@ -1,3 +1,13 @@
 from fastapi import APIRouter
 
+from mecsa_erp.area_operaciones.modulo0.routing import router as modulo0_router
+from mecsa_erp.area_operaciones.modulo1.routing import router as modulo1_router
+from mecsa_erp.area_operaciones.modulo2.routing import router as modulo2_router
+from mecsa_erp.usuarios.routing import router as usuarios_router
+
 api_router = APIRouter()
+
+api_router.include_router(modulo0_router)
+api_router.include_router(modulo1_router)
+api_router.include_router(modulo2_router)
+api_router.include_router(usuarios_router)
