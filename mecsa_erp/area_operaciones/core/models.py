9bb831caebc3c329736546a2f39394817d5bc569
JohanTv <johan.tanta@utec.edu.pe>
2024-04-16 23:30:43 -0500

    add(core.models): add models for Movimiento, MovimientoDescripcion, Almacen, MovimientoIngreso, MovimientoIngresoOrdenCompra

--- mecsa_erp/area_operaciones/core/models.py
+++ mecsa_erp/area_operaciones/core/models.py
@@ -1,5 +1,7 @@
+from sqlalchemy import Column, CheckConstraint, CHAR, DATE, func
 from sqlmodel import SQLModel, Field
 from typing import Optional
+from datetime import date
 
 class Proveedor(SQLModel, table=True):
     __tablename__ = "proveedor"
@@ -24,3 +26,38 @@ class OrdenCompraDetalle(SQLModel, table=True):
     
     orden_compra_id: str = Field(primary_key=True, foreign_key="orden_compra.orden_compra_id")
     producto_id: str = Field(primary_key=True, foreign_key="producto.producto_id")
+    
+class Movimiento(SQLModel, table=True):
+    __tablename__ = "movimiento"
+    
+    movimiento_id: str = Field(primary_key=True)
+    movimiento_descripcion_id: str = Field(foreign_key="movimiento_descripcion.movimiento_descripcion_id")
+    proveedor_id: str = Field(foreign_key="proveedor.proveedor_id")
+    fecha: date = Field(sa_column=Column(DATE, server_default=func.current_date(), nullable=False)) 
+    
+class MovimientoDescripcion(SQLModel, table=True):
+    __tablename__ = "movimiento_descripcion"
+    
+    movimiento_descripcion_id: str = Field(primary_key=True)
+    movimiento_tipo: str = Field(sa_type=CHAR, sa_column_args=[CheckConstraint("movimiento_tipo IN ('I', 'S')")])
+    almacen_id: str = Field(foreign_key="almacen.almacen_id")
+    descripcion: str
+    
+class Almacen(SQLModel, table=True):
+    __tablename__ = "almacen"
+    
+    almacen_id: str = Field(primary_key=True)
+    nombre: str
+    
+class MovimientoIngreso(SQLModel, table=True):
+    __tablename__ = "movimiento_ingreso"
+    
+    movimiento_ingreso_id: str = Field(primary_key=True, foreign_key="movimiento.movimiento_id")
+    guia_proveedor_nro_serie: str
+    guia_proveedor_nro_correlativo: str
+
+class MovimientoIngresoOrdenCompra(SQLModel, table=True):
+    __tablename__ = "movimiento_ingreso_orden_compra"
+    
+    movimiento_ingreso_id: str = Field(primary_key=True, foreign_key="movimiento_ingreso.movimiento_ingreso_id")
+    orden_compra_id: str = Field(primary_key=True, foreign_key="orden_compra.orden_compra_id")
