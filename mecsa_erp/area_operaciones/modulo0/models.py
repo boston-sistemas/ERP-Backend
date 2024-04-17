d9cfa95f7d8b8aadb0dd4b58d651c94d0125c256
JohanTv <johan.tanta@utec.edu.pe>
2024-04-16 23:33:53 -0500

    add(modulo0.models): add some 'modulo0' module  models

--- mecsa_erp/area_operaciones/modulo0/models.py
+++ mecsa_erp/area_operaciones/modulo0/models.py
@@ -1,10 +1,69 @@
+from sqlalchemy import Boolean, ForeignKey, Numeric
 from sqlmodel import SQLModel, Field
 
 class Hilado(SQLModel, table=True):
     __tablename__ = "hilado"
     
-    hilado_id: str = Field(primary_key=True, foreign_key="producto.producto_id")
+    hilado_id: str = Field(primary_key=True, sa_column_args=[ForeignKey("producto.producto_id")])
     titulo: str
     fibra: str
     procedencia: str
     acabado: str
+    
+class Tejido(SQLModel, table=True):
+    __tablename__ = "tejido"
+    
+    tejido_id: str = Field(primary_key=True)
+    nombre: str
+
+class Crudo(SQLModel, table=True):
+    __tablename__ = "crudo"
+    
+    crudo_id: str = Field(primary_key=True)
+    tejido_id: str = Field(sa_column_args=[ForeignKey("tejido.tejido_id")])
+    densidad: int
+    ancho: int
+    galga: int
+    diametro: int
+    longitud_malla: float = Field(sa_type=Numeric)
+ 
+class MovimientoIngresoHiladoDetalle(SQLModel, table=True):
+    __tablename__ = "movimiento_ingreso_hilado_detalle"
+    
+    movimiento_ingreso_id: str = Field(primary_key=True, sa_column_args=[ForeignKey("movimiento.movimiento_id")])
+    hilado_id: str = Field(sa_column_args=[ForeignKey("hilado.hilado_id")])
+    nro_bultos: int
+    nro_conos: int
+    cantidad_kg: float = Field(sa_type=Numeric)
+    codigo_lote: str
+
+class OrdenServicioTejeduria(SQLModel, table=True):
+    __tablename__ = "orden_servicio_tejeduria"
+    
+    orden_servicio_tejeduria_id: str = Field(primary_key=True)
+    tejeduria_id: str = Field(sa_column_args=[ForeignKey("proveedor.proveedor_id")])
+    # estado
+    
+class MovimientoSalidaHilado(SQLModel, table=True):
+    __tablename__ = "movimiento_salida_hilado"
+    
+    movimiento_salida_id: str = Field(primary_key=True, sa_column_args=[ForeignKey("movimiento.movimiento_id")])
+    orden_servicio_tejeduria_id: str = Field(sa_column_args=[ForeignKey("orden_servicio_tejeduria.orden_servicio_tejeduria_id")])
+    
+class MovimientoSalidaHiladoDetalle(SQLModel, table=True):
+    __tablename__ = "movimiento_salida_hilado_detalle"
+    
+    movimiento_salida_id: str = Field(primary_key=True, sa_column_args=[ForeignKey("movimiento.movimiento_id")])
+    movimiento_ingreso_id: str = Field(primary_key=True, sa_column_args=[ForeignKey("movimiento.movimiento_id")])
+    hilado_id: str = Field(sa_column_args=[ForeignKey("hilado.hilado_id")])
+    nro_bultos: int
+    nro_conos: int
+    cantidad_kg: float = Field(sa_type=Numeric)
+    
+class OrdenServicioTejeduriaDetalle(SQLModel, table=True):
+    __tablename__ = "orden_servicio_tejeduria_detalle"
+    
+    orden_servicio_tejeduria_id: str = Field(primary_key=True, sa_column_args=[ForeignKey("orden_servicio_tejeduria.orden_servicio_tejeduria_id")])
+    crudo_id: str = Field(primary_key=True, sa_column_args=[ForeignKey("crudo.crudo_id")])
+    cantidad_kg: float = Field(sa_type=Numeric)
+    es_complemento: bool = Field(sa_type=Boolean)
