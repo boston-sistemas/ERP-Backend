2c1911bd5c0a23369cdb0f63a9e11abc2966a2fd
JohanTv <johan.tanta@utec.edu.pe>
2024-04-19 10:02:42 -0500

    refactor: rename function get_db to get_session

--- config/database.py
+++ config/database.py
@@ -6,6 +6,6 @@ from settings import settings
 
 engine = create_engine(settings.DATABASE_URL)
 
-def get_db() -> Generator[Session, None, None]:
+def get_session() -> Generator[Session, None, None]:
     with Session(engine) as session:
         yield session
