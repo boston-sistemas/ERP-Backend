8ef9a108e4d66e430dff5d42d53fc9e7c7323f9b
JohanTv <johan.tanta@utec.edu.pe>
2024-04-19 15:35:00 -0500

    feat(modulo0.routing): Add route for retrieving data as a test

--- mecsa_erp/area_operaciones/modulo0/routing.py
+++ mecsa_erp/area_operaciones/modulo0/routing.py
@@ -1,3 +1,16 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from sqlmodel import SQLModel, Session, select
+
+from config.database import get_session
+from mecsa_erp.area_operaciones.modulo0.models import OrdenServicioTejeduriaDetalle
 
 router = APIRouter(tags=["Módulo 0"], prefix="/modulo0")
+
+class OrdenServicioTejeduriaDetalleList(SQLModel):
+    data: list[OrdenServicioTejeduriaDetalle]
+
+@router.get("/orden-servicio-tejeduria-detalle", response_model=OrdenServicioTejeduriaDetalleList)
+def get_orden_servicio_tejeduria_detalle(session: Session = Depends(get_session)):
+    statement = select(OrdenServicioTejeduriaDetalle)
+    objects = session.exec(statement).all()
+    return OrdenServicioTejeduriaDetalleList(data=objects)
