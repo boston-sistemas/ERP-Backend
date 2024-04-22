41761066ceda2f5bb7a24442e6962a5ae0bcfdf4
AaronCoorahua <ruben.coorahua@utec.edu.pe>
2024-04-22 15:40:40 -0500

    Primer modelo relacional del modulo usuarios

--- mecsa_erp/usuarios/models.py
+++ mecsa_erp/usuarios/models.py
@@ -1,1 +1,56 @@
-from sqlmodel import SQLModel
+from sqlalchemy import ForeignKeyConstraint
+from sqlmodel import SQLModel, Field
+
+class Usuario(SQLModel, table=True):
+    __tablename__ = "usuario"
+    
+    username: str = Field(primary_key=True)
+    password: str
+    email: str
+    display_name: str
+
+
+class Rol(SQLModel, table=True):
+    __tablename__ = "rol"
+
+    rol_id: int = Field(primary_key=True)
+    nombre: str
+
+class UsuarioRol(SQLModel, table=True):
+    __tablename__ = "usuario_rol"
+
+    usuario_id: str = Field(primary_key=True)
+    rol_id: int = Field(primary_key=True)
+ 
+    __table_args__ = (
+        ForeignKeyConstraint(['usuario_id'], ['usuario.username']),
+        ForeignKeyConstraint(['rol_id'], ['rol.rol_id'])
+    )
+
+class UsuarioAcceso(SQLModel, table=True):
+    __tablename__ = "usuario_acceso"
+
+    usuario_id: str = Field(primary_key=True)
+    acceso_id: int = Field(primary_key=True)
+
+    __table_args__ = (
+        ForeignKeyConstraint(['usuario_id'], ['usuario.username']),
+        ForeignKeyConstraint(['acceso_id'], ['acceso.acceso_id'])
+    )
+
+class Acceso(SQLModel, table=True):
+    __tablename__ = "acceso"
+
+    acceso_id: int = Field(primary_key=True)
+    nombre: str
+    
+class Rol_acceso(SQLModel, table=True):
+    __tablename__ = "rol_acceso"
+
+    rol_id: int = Field(primary_key=True)
+    acceso_id: int = Field(primary_key=True)
+
+    __table_args__ = (
+        ForeignKeyConstraint(['rol_id'], ['rol.rol_id']),
+        ForeignKeyConstraint(['acceso_id'], ['acceso.acceso_id'])
+    )
