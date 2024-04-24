4f2d5cdc8d8836bd5e2aa9293e97cb3b61f47be2
JohanTv <johan.tanta@utec.edu.pe>
2024-04-24 00:44:18 -0500

    feat(config.database): add SessionDependency declaration for endpoints

--- config/database.py
+++ config/database.py
@@ -1,11 +1,17 @@
 from collections.abc import Generator
+from typing import Annotated
 
+from fastapi import Depends
 from sqlmodel import Session, create_engine
 
 from config.settings import settings
 
 engine = create_engine(settings.DATABASE_URL)
 
+
 def get_session() -> Generator[Session, None, None]:
     with Session(engine) as session:
         yield session
+
+
+SessionDependency = Annotated[Session, Depends(get_session)]
