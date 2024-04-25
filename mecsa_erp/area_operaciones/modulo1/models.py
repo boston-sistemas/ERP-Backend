ba0661ecd3cdb417504e53a7997e18614fdf5831
JohanTv <johan.tanta@utec.edu.pe>
2024-04-25 10:13:43 -0500

    refactor(models): refactorize OrdenServicioTejeduriaDetalleReporteLog model

--- mecsa_erp/area_operaciones/modulo1/models.py
+++ mecsa_erp/area_operaciones/modulo1/models.py
@@ -1,18 +1,19 @@
-from decimal import Decimal
-from typing import Optional
-from uuid import UUID
 from sqlalchemy import TIMESTAMP, Column, Numeric, func
-from sqlmodel import Field, SQLModel
+from sqlmodel import SQLModel, Field
+
 from datetime import datetime
+from decimal import Decimal
+from uuid import UUID
+
 
 class OrdenServicioTejeduriaDetalleReporteLog(SQLModel, table=True):
     __tablename__ = "orden_servicio_tejeduria_detalle_reporte_log"
 
-    log_id: Optional[UUID] = Field(primary_key=True)
+    log_id: UUID = Field(primary_key=True)
     proveedor_id: str
     orden_servicio_tejeduria_id: str
     crudo_id: str
     estado: str
     reporte_tejeduria_nro_rollos: int
     reporte_tejeduria_cantidad_kg: Decimal = Field(sa_type=Numeric)
-    timestamp: Optional[datetime] = Field(sa_column=Column(TIMESTAMP, server_default=func.now(), nullable=False))
+    timestamp: datetime = Field(sa_column=Column(TIMESTAMP, server_default=func.now()))
