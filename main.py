91dc7d255d92e244e57eb9203fcc53fc4efc5e75
AaronCoorahua <ruben.coorahua@utec.edu.pe>
2024-04-30 16:03:18 -0500

    cors habilitado para todo

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://127.0.0.1:3000", "http://localhost:3000"],  
+    allow_origins=["*"],  
     allow_credentials=True,
     allow_methods=["*"],  
     allow_headers=["*"],  
