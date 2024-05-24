b1b39d78fe33c3faa470c9ae0f030adc1fa35010
JohanTv <johan.tanta@utec.edu.pe>
2024-05-24 17:01:53 -0500

    feat(usuarios.api): add endpoints to manage role access (add and remove)

--- mecsa_erp/usuarios/api/rol.py
+++ mecsa_erp/usuarios/api/rol.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, HTTPException, status
+from fastapi import APIRouter, Body, HTTPException, status
 from sqlalchemy.orm import joinedload
 
 from config.database import SessionDependency
@@ -6,7 +6,11 @@ from helpers.crud import CRUD
 
 from mecsa_erp.usuarios.models import Acceso, Rol, RolAcceso
 from mecsa_erp.usuarios.schemas.usuario import (
-    RolCreateSchema, RolListSchema, RolSchema, RolUpdateSchema)
+    RolCreateSchema,
+    RolListSchema,
+    RolSchema,
+    RolUpdateSchema,
+)
 
 crud_acceso = CRUD[Acceso, Acceso](Acceso)
 crud_rol = CRUD[Rol, RolCreateSchema](Rol)
@@ -14,11 +18,10 @@ crud_rol_acceso = CRUD[RolAcceso, RolAcceso](RolAcceso)
 
 router = APIRouter(tags=["Roles"], prefix="/roles")
 
+
 @router.get("/{rol_id}", response_model=RolSchema)
 def get_rol(session: SessionDependency, rol_id: int):
-    rol = crud_rol.get_by_pk_or_404(
-        session, rol_id, [joinedload(Rol.accesos)]
-    )
+    rol = crud_rol.get_by_pk_or_404(session, rol_id, [joinedload(Rol.accesos)])
     return rol
 
 
@@ -38,9 +41,9 @@ def create_rol(session: SessionDependency, rol: RolCreateSchema):
             status_code=status.HTTP_409_CONFLICT,
             detail=f"Rol {rol.nombre} ya existe",
         )
-    
+
     message, db_rol = crud_rol.create(session, rol, commit=False)
-    
+
     if rol.acceso_ids:
         for acceso_id in set(rol.acceso_ids):
             acceso = crud_acceso.get_by_pk_or_404(session, acceso_id)
@@ -50,14 +53,12 @@ def create_rol(session: SessionDependency, rol: RolCreateSchema):
                 commit=False,
             )
 
-
     session.commit()
     return {"message": message}
 
+
 @router.put("/{rol_id}")
-def update_rol(
-    session: SessionDependency, rol_id: int, rol: RolUpdateSchema
-):
+def update_rol(session: SessionDependency, rol_id: int, rol: RolUpdateSchema):
     db_rol = crud_rol.get_by_pk_or_404(session, rol_id)
 
     exists = rol.nombre and crud_rol.get(session, Rol.nombre == rol.nombre)
@@ -67,14 +68,63 @@ def update_rol(
             detail=f"Rol {rol.nombre} ya existe",
         )
 
-    message, _ = crud_rol.update(
-        session, db_rol, rol.model_dump(exclude_unset=True)
-    )
+    message, _ = crud_rol.update(session, db_rol, rol.model_dump(exclude_unset=True))
 
     return {"message": message}
+
 
 @router.delete("/{rol_id}")
 def delete_usuario(session: SessionDependency, rol_id: int):
     rol = crud_rol.get_by_pk_or_404(session, rol_id)
     message = crud_rol.delete(session, rol)
     return {"message": message}
+
+
+#######################################################
+
+
+@router.post("/{rol_id}/accesos/")
+def add_accesos_to_rol(
+    session: SessionDependency, rol_id: int, acceso_ids: list[int] = Body(embed=True)
+):
+    rol = crud_rol.get_by_pk_or_404(session, rol_id)
+    for acceso_id in set(acceso_ids):
+        acceso = crud_acceso.get_by_pk_or_404(session, acceso_id)
+        exists = crud_rol_acceso.get_by_pk(
+            session, {"rol_id": rol.rol_id, "acceso_id": acceso.acceso_id}
+        )
+
+        if exists:
+            raise HTTPException(
+                status_code=status.HTTP_409_CONFLICT,
+                detail=f"Rol {rol.nombre} ya tiene el acceso: {acceso.nombre}",
+            )
+        crud_rol_acceso.create(
+            session,
+            RolAcceso(rol_id=rol.rol_id, acceso_id=acceso.acceso_id),
+            commit=False,
+        )
+
+    session.commit()
+    return {"message": "Accesos añadidos"}
+
+
+@router.delete("/{rol_id}/accesos/")
+def delete_accesos_from_rol(
+    session: SessionDependency, rol_id: int, acceso_ids: list[int] = Body(embed=True)
+):
+    rol = crud_rol.get_by_pk_or_404(session, rol_id)
+    for acceso_id in set(acceso_ids):
+        rol_acceso = crud_rol_acceso.get_by_pk(
+            session, {"rol_id": rol_id, "acceso_id": acceso_id}
+        )
+
+        if not rol_acceso:
+            raise HTTPException(
+                status_code=status.HTTP_409_CONFLICT,
+                detail=f"Rol {rol.nombre} no tiene el acceso con ID: {acceso_id}",
+            )
+        session.delete(rol_acceso)
+
+    session.commit()
+    return {"message": "Accesos eliminados"}
