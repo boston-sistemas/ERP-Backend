b7e626bd2eb3f3091d0382d666b271d1b71b5e10
JohanTv <johan.tanta@utec.edu.pe>
2024-06-03 12:13:46 -0500

    feat: add reporte-stock/subordenes endpoint

--- mecsa_erp/area_operaciones/modulo1/routing.py
+++ mecsa_erp/area_operaciones/modulo1/routing.py
@@ -2,17 +2,28 @@ from fastapi import APIRouter
 
 from config.database import SessionDependency
 from helpers.crud import CRUD
-from mecsa_erp.area_operaciones.modulo0.models import OrdenServicioTejeduria
-from mecsa_erp.area_operaciones.modulo1.schemas import RevisionStock, ReporteStock
+from mecsa_erp.area_operaciones.modulo0.models import (
+    OrdenServicioTejeduria,
+    OrdenServicioTejeduriaDetalle,
+)
+from mecsa_erp.area_operaciones.modulo1.schemas import (
+    OrdenServicioTejeduriaDetalleListUpdateSchema,
+    OrdenServicioTejeduriaUpdateSchema,
+    RevisionStock,
+    ReporteStock,
+)
 
 crud_orden_servicio_tejeduria = CRUD[OrdenServicioTejeduria, OrdenServicioTejeduria](
     OrdenServicioTejeduria
 )
+crud_orden_servicio_detalle_tejeduria = CRUD[
+    OrdenServicioTejeduriaDetalle, OrdenServicioTejeduriaDetalle
+](OrdenServicioTejeduriaDetalle)
 
 router = APIRouter(prefix="/operations/v1", tags=["Area Operaciones"])
 
 
-@router.get("/reporte-stock")
+@router.get("/reporte-stock", response_model=ReporteStock)
 def reporte_stock(session: SessionDependency):
     # TODO: Encontrar el codigo del proveedor asociado al usuario
     proveedor_id = "RSA"
@@ -26,6 +37,38 @@ def reporte_stock(session: SessionDependency):
 
     items = [detalle for orden in ordenes for detalle in orden.detalles]
     return ReporteStock(subordenes=items)
+
+
+@router.put("/reporte-stock/subordenes")
+def reporte_stock_update_suborders(
+    body: OrdenServicioTejeduriaDetalleListUpdateSchema, session: SessionDependency
+):
+    subordenes = body.subordenes
+    for suborden in subordenes:
+        db_suborden = crud_orden_servicio_detalle_tejeduria.get_by_pk_or_404(
+            session,
+            {
+                "orden_servicio_tejeduria_id": suborden.orden_servicio_tejeduria_id,
+                "crudo_id": suborden.crudo_id,
+            },
+        )
+        crud_orden_servicio_detalle_tejeduria.update(
+            session,
+            db_suborden,
+            suborden.model_dump(
+                exclude={"orden_servicio_tejeduria_id", "crudo_id"}, exclude_unset=True
+            ),
+            commit=False,
+        )
+    session.commit()
+    return {"message": "Subordenes actualizadas"}
+
+
+@router.put("/reporte-stock/ordenes")
+def reporte_stock_update_orders(
+    orders: list[OrdenServicioTejeduriaUpdateSchema], session: SessionDependency
+):
+    pass
 
 
 @router.get("/revision-stock")
--- mecsa_erp/area_operaciones/modulo1/schemas.py
+++ mecsa_erp/area_operaciones/modulo1/schemas.py
@@ -1,4 +1,5 @@
-from sqlmodel import SQLModel
+from enum import Enum
+from sqlmodel import Field, SQLModel
 
 from mecsa_erp.area_operaciones.modulo0.schemas.orden_servicio_tejeduria import (
     OrdenServicioTejeduriaSchema,
@@ -15,3 +16,42 @@ class ReporteStock(SQLModel):
 class RevisionStock(SQLModel):
     ordenes_pendientes: list[OrdenServicioTejeduriaSchema]
     ordenes_cerradas: list[OrdenServicioTejeduriaSchema]
+
+
+class OrdenServicioTejeduriaEstadoEnum(str, Enum):
+    pendiente = "PENDIENTE"
+    cerrado = "CERRADO"
+    liquidado = "LIQUIDADO"
+
+
+class OrdenServicioTejeduriaDetalleEstadoEnum(str, Enum):
+    no_iniciado = "NO INICIADO"
+    cerrado = "EN CURSO"
+    liquidado = "DETENIDO"
+    listo = "LISTO"
+
+
+class OrdenServicioTejeduriaIDSchema(SQLModel):
+    orden_servicio_tejeduria_id: str
+    crudo_id: str
+
+
+class OrdenServicioTejeduriaDetalleUpdateSchema(SQLModel):
+    reporte_tejeduria_nro_rollos: int | None = Field(default=None, ge=0)
+    reporte_tejeduria_cantidad_kg: float | None = Field(default=None, ge=0)
+    estado: OrdenServicioTejeduriaDetalleEstadoEnum | None = None
+
+
+class OrdenServicioTejeduriaSchema(
+    OrdenServicioTejeduriaDetalleUpdateSchema, OrdenServicioTejeduriaIDSchema
+):
+    pass
+
+
+class OrdenServicioTejeduriaDetalleListUpdateSchema(SQLModel):
+    subordenes: list[OrdenServicioTejeduriaSchema]
+
+
+class OrdenServicioTejeduriaUpdateSchema(SQLModel):
+    orden_servicio_tejeduria_id: str
+    estado: OrdenServicioTejeduriaEstadoEnum
