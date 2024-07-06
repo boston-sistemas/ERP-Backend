33e546b10ad9c6f7ed10b0b5898b7d62d721c937
JohanTv <johan.tanta@utec.edu.pe>
2024-07-06 02:57:49 -0500

    chore(core): update get_db() to use AsyncGenerator

--- src/core/database.py
+++ src/core/database.py
@@ -1,14 +1,18 @@
 from collections.abc import Generator
-from typing import Annotated
+from typing import Annotated, AsyncGenerator
 
 from fastapi import Depends
+from sqlalchemy import create_engine
 from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
-from sqlalchemy.orm import DeclarativeBase
-from sqlmodel import Session, create_engine
+from sqlalchemy.orm import DeclarativeBase, Session
 
 from src.core.config import settings
 
 engine = create_engine(settings.DATABASE_URL, max_identifier_length=60)
+engine_async = create_async_engine(settings.DATABASE_URL_ASYNC)
+AsyncSessionLocal = async_sessionmaker(
+    bind=engine_async, class_=AsyncSession, expire_on_commit=False
+)
 
 
 class Base(DeclarativeBase):
@@ -20,15 +24,9 @@ def get_session() -> Generator[Session, None, None]:
         yield session
 
 
-engine_async = create_async_engine(settings.DATABASE_URL_ASYNC)
-AsyncSessionLocal = async_sessionmaker(
-    bind=engine_async, class_=AsyncSession, expire_on_commit=False
-)
-
-
-async def get_db() -> Generator[AsyncSession, None, None]:
-    async with AsyncSessionLocal() as session:
-        yield session
+async def get_db() -> AsyncGenerator[AsyncSession, None]:
+    async with AsyncSessionLocal() as db:
+        yield db
 
 
 SessionDependency = Annotated[Session, Depends(get_db)]
