f491c2aea67825051c5a131f76f69a4d907089dc
JohanTv <johan.tanta@utec.edu.pe>
2024-07-08 16:26:12 -0500

    feat(operations): add ReporteStockTejeduriaService class

--- src/operations/services/__init__.py
+++ src/operations/services/__init__.py
@@ -2,5 +2,10 @@ from .orden_servicio_tejeduria_detalle_service import (
     OrdenServicioTejeduriaDetalleService,
 )
 from .orden_servicio_tejeduria_service import OrdenServicioTejeduriaService
+from .reporte_stock_tejeduria_service import ReporteStockTejeduriaService
 
-__all__ = ["OrdenServicioTejeduriaService", "OrdenServicioTejeduriaDetalleService"]
+__all__ = [
+    "OrdenServicioTejeduriaService",
+    "OrdenServicioTejeduriaDetalleService",
+    "ReporteStockTejeduriaService",
+]
