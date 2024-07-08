6d72a6e20e0e48b73e1fc27fd5ca00ec04b5c74e
JohanTv <johan.tanta@utec.edu.pe>
2024-07-07 19:00:41 -0500

    refactor(security): rename routers files and update routers import
    - Renamed routers files to follow the new naming convention
    - Improved code organization and clarity

--- src/security/routing.py
+++ src/security/routing.py
@@ -1,9 +1,9 @@
 from fastapi import APIRouter
 
-from src.security.routers import acceso_router, auth, rol_router, user_router
+from src.security.routers import acceso_router, auth_router, rol_router, user_router
 
 router = APIRouter(prefix="/security/v1")  # tags=["Area Seguridad"]
-router.include_router(auth.router)
-router.include_router(user_router)
-router.include_router(rol_router)
-router.include_router(acceso_router)
+router.include_router(auth_router.router)
+router.include_router(user_router.router)
+router.include_router(rol_router.router)
+router.include_router(acceso_router.router)
