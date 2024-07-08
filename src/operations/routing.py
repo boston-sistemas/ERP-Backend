5a3358825fc915d3900dd35264773c962508c738
JohanTv <johan.tanta@utec.edu.pe>
2024-07-08 18:08:13 -0500

    feat(operations): include ReporteStockTejeduria and RevisionStockTejeduria routers

--- src/operations/routing.py
+++ src/operations/routing.py
@@ -1,8 +1,14 @@
 from fastapi import APIRouter
 
-from .routers import programacion_tintoreria
+from .routers import (
+    programacion_tintoreria,
+    reporte_stock_tejeduria_router,
+    revision_stock_tejeduria_router,
+)
 
 router = APIRouter(prefix="/operations/v1")  # tags=["Area Operaciones"]
+router.include_router(reporte_stock_tejeduria_router.router)
+router.include_router(revision_stock_tejeduria_router.router)
 router.include_router(
     programacion_tintoreria.router, tags=["Area Operaciones - Programacion Tintoreria"]
 )
