ce91947587c4fe3c9a5d75eeeee35c73bca0b342
JohanTv <johan.tanta@utec.edu.pe>
2024-07-08 13:08:09 -0500

    feat(operations): add OrdenServicioTejeduriaDetalle class

---
+++ src/operations/repositories/__init__.py
@@ -0,0 +1,9 @@
+from .orden_servicio_tejeduria_detalle_repository import (
+    OrdenServicioTejeduriaDetalleRepository,
+)
+from .orden_servicio_tejeduria_repository import OrdenServicioTejeduriaRepository
+
+__all__ = [
+    "OrdenServicioTejeduriaRepository",
+    "OrdenServicioTejeduriaDetalleRepository",
+]
