934d53e82a2fb71c0be7c5436d1e98e35cecdd5e
JohanTv <johan.tanta@utec.edu.pe>
2024-07-08 13:08:53 -0500

    feat(operations): add OrdenServicioTejeduriaDetalleFailures class

---
+++ src/operations/failures/__init__.py
@@ -0,0 +1,6 @@
+from .orden_servicio_tejeduria_detalle_failures import (
+    OrdenServicioTejeduriaDetalleFailures,
+)
+from .orden_servicio_tejeduria_failures import OrdenServicioTejeduriaFailures
+
+__all__ = ["OrdenServicioTejeduriaFailures", "OrdenServicioTejeduriaDetalleFailures"]
