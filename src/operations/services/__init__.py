8e87d26a0c33a7cf73670a6f3f96e7f6f9ac7f3b
JohanTv <johan.tanta@utec.edu.pe>
2024-07-10 17:16:26 -0500

    feat(operations): add OrdenServicioTintoreriaService class

--- src/operations/services/__init__.py
+++ src/operations/services/__init__.py
@@ -7,12 +7,14 @@ from .orden_servicio_tejeduria_service import OrdenServicioTejeduriaService
 from .orden_servicio_tintoreria_detalle_service import (
     OrdenServicioTintoreriaDetalleService,
 )
+from .orden_servicio_tintoreria_service import OrdenServicioTintoreriaService
 from .proveedor_service import ProveedorService
 from .reporte_stock_tejeduria_service import ReporteStockTejeduriaService
 from .revision_stock_tejeduria_service import RevisionStockTejeduriaService
 
 __all__ = [
     "OrdenServicioTintoreriaDetalleService",
+    "OrdenServicioTintoreriaService",
     "OrdenServicioTejeduriaService",
     "OrdenServicioTejeduriaDetalleService",
     "ReporteStockTejeduriaService",
