90a3f1bfee2c2639ac933000d0cd4d9d783c430a
JohanTv <johan.tanta@utec.edu.pe>
2024-07-12 14:51:40 -0500

    feat(operations): add programacion-tintoreria/ POST endpoint

--- src/operations/routers/programacion_tintoreria_router.py
+++ src/operations/routers/programacion_tintoreria_router.py
@@ -3,6 +3,7 @@ from sqlalchemy.ext.asyncio import AsyncSession
 
 from src.core.database import get_db
 from src.operations.schemas import (
+    ProgramacionTintoreriaCreateSchema,
     ProgramacionTintoreriaParametersResponse,
 )
 from src.operations.services import ProgramacionTintoreriaService
@@ -15,9 +16,21 @@ router = APIRouter(
 
 @router.get("/parametros", response_model=ProgramacionTintoreriaParametersResponse)
 async def retrieve_parameters(db: AsyncSession = Depends(get_db)):
-    programacion_tintoreria = ProgramacionTintoreriaService(db)
-    result = await programacion_tintoreria.retrieve_parameters()
+    programacion_service = ProgramacionTintoreriaService(db)
+    result = await programacion_service.retrieve_parameters()
     if result.is_success:
         return result.value
 
     raise result.error
+
+
+@router.post("/")
+async def create_programacion_tintoreria(
+    programacion: ProgramacionTintoreriaCreateSchema, db: AsyncSession = Depends(get_db)
+):
+    programacion_service = ProgramacionTintoreriaService(db)
+    creation_result = await programacion_service.create_programacion(programacion)
+    if creation_result.is_success:
+        return {"message": "Programacion de tintorería hecha y enviada."}
+
+    raise creation_result.error
--- src/operations/services/orden_servicio_tintoreria_service.py
+++ src/operations/services/orden_servicio_tintoreria_service.py
@@ -1,4 +1,3 @@
-
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from src.core.database import transactional
