4c23620fdf7f4ca833d605b99cb14a3c8366a75c
Luis-Golac <luis.golac@utec.edu.pe>
2024-08-12 14:32:22 -0500

    refactor(operations): add utils folder and clean up code

--- src/core/services/email_service.py
+++ src/core/services/email_service.py
@@ -9,81 +9,18 @@ from jinja2 import Environment, FileSystemLoader
 from reportlab.lib.pagesizes import landscape, letter
 
 from src.core.config import settings
-from src.core.services.pdf_service import PDFService, TableStyle, colors, inch
+from src.core.utils.operaciones_tintoreria_pdf import (
+    TableStyle,
+    colors,
+    generate_pdf_programacion,
+    inch,
+)
 
 timezone = pytz.timezone("America/Lima")
 
 LOGO_MECSA = "https://lh3.googleusercontent.com/pw/AP1GczOxb5h_TPjSWvXctIscyr_Yedt7H2ck4BJMH_8iuedQOxo0g-kRtWkDlJiQuIU6-6zDRaw00vFLTcuMlyi5_uiG17-yiD4WdtOhRs1Q2lunl_sr11qSdsK5fozwLoxaANW2ycTRPjVZPW8e3KsV27s=w1920-h610-s-no-gm"
 
 PROGRAMACION_TINTORERIA_ASUNTO = "PROGRAMACIÓN TINTORERÍA - SEMANA {0}"
-PROGRAMACION_TINTORERIA_HTML = """
-<!DOCTYPE html>
-<html>
-<head>
-    <style>
-        @import url('https://fonts.googleapis.com/css2?family=Nunito:wght@400;700&display=swap');
-
-        body {{
-            font-family: 'Nunito', sans-serif;
-        }}
-
-        table {{
-            width: 100%;
-            border-collapse: collapse;
-        }}
-
-        th, td {{
-            border: 1px solid black;
-            padding: 8px;
-            text-align: center;
-        }}
-
-        th {{
-            background-color: #FFCC00;
-            font-weight: bold;
-            text-align: center;
-        }}
-
-        .header-row {{
-            background-color: #FFCC00;
-            text-align: center;
-        }}
-
-        .partida-cell {{
-            font-weight: bold;
-        }}
-
-        .number-cell {{
-            text-align: center;
-            font-weight: bold;
-        }}
-    </style>
-</head>
-<body>
-    <p> Buenos días, {0} <p>
-    <p>Por favor preparar la siguiente relación de tejido para su despacho, recoge {6}<p>
-    <p>Muchas gracias<p>
-    <strong>Area de Operaciones</strong>
-    <p><strong>Fecha: </strong>{1}<p>
-    <table>
-        <thead>
-            <tr class="header-row">
-                {2}
-            </tr>
-        </thead>
-        <tbody>
-            {3}
-            <tr style="background-color: #111;">
-                <td colspan="4" style="text-align: center; font-weight: bold; color: white;">Totales</td>
-                <td style="text-align: center; font-weight: bold; color: white;">{4}</td>
-                <td style="text-align: center; font-weight: bold; color: white;">{5}</td>
-                <td colspan="2" style="color: white;"></td>
-            </tr>
-        </tbody>
-    </table>
-</body>
-</html>
-"""
 
 
 class EmailService:
@@ -92,7 +29,6 @@ class EmailService:
         self.template_env = Environment(
             loader=FileSystemLoader(searchpath=settings.ASSETS_DIR + "email_templates")
         )
-        self.pdf_service = PDFService()
 
     @staticmethod
     def data_processing(data: dict, lengths: dict, style: TableStyle):
@@ -218,21 +154,23 @@ class EmailService:
         email = resend.Emails.send(params)
         return email
 
-    async def send_email_pdf(
+    async def send_email_with_pdf(
         self,
         name_pdf: str,
         name_pdf_send: str,
         fecha: str,
         data: dict,
     ):
-        body_email = PROGRAMACION_TINTORERIA_HTML.format(
-            data["from"],
-            fecha,
-            data["title"],
-            data["values"],
-            data["rolls"],
-            data["weights"],
-            data["to"],
+        template = self.template_env.get_template("send_programming_dry_cleaners.html")
+
+        html_content = template.render(
+            from_=data["from"],
+            to=data["to"],
+            date=fecha,
+            titles=data["title"],
+            values=data["values"],
+            rolls=data["rolls"],
+            weights=data["weights"],
         )
 
         with open(name_pdf, "rb") as pdf_file:
@@ -246,7 +184,7 @@ class EmailService:
             "from": "practicante.sistemas@boston.com.pe",
             "to": data["email_to"],
             "subject": subject_email,
-            "html": body_email,
+            "html": html_content,
             "attachments": [
                 {
                     "filename": name_pdf_send,
@@ -321,7 +259,7 @@ class EmailService:
         data_table_pdf = data
         data_table_pdf["title"] = titles_pdf
         data_table_pdf["values"] = values_pdf
-        self.pdf_service.generate_pdf(
+        generate_pdf_programacion(
             name_pdf,
             image_path,
             reduction_percentaje,
@@ -339,7 +277,7 @@ class EmailService:
         data_table_email["rolls"] = total_rolls
         data_table_email["weights"] = total_weight
 
-        await self.send_email_pdf(name_pdf, "reporte.pdf", fecha, data_table_email)
+        await self.send_email_with_pdf(name_pdf, "reporte.pdf", fecha, data_table_email)
 
     async def send_welcome_email(
         self, email_to: str, display_name: str, username: str, password: str
