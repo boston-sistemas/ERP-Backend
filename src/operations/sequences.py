759e7153e16d167c661093af0c87dad3f4773af5
JohanTv <johan.tanta@utec.edu.pe>
2024-11-07 15:24:47 -0500

    feat(operations): add color_id sequence

--- src/operations/sequences.py
+++ src/operations/sequences.py
@@ -5,3 +5,7 @@ from src.core.database import Base
 product_id_seq = Sequence(
     "product_id_seq", start=100100100100, maxvalue=999999999999, metadata=Base.metadata
 )
+
+color_id_seq = Sequence(
+    "color_id_seq", start=100100, maxvalue=999999, metadata=Base.metadata
+)
