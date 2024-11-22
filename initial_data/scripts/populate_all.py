0c7c237c4b501660e58a16bd7e1677adc505f888
JohanTv <johan.tanta@utec.edu.pe>
2024-11-22 01:15:26 -0500

    chore(initial_data): ensure proper disposal of database sessions

--- initial_data/scripts/populate_all.py
+++ initial_data/scripts/populate_all.py
@@ -6,6 +6,8 @@ from populate_mecsa_colors import populate_mecsa_colors
 from populate_parameter_categories import populate_parameter_categories
 from populate_parameters import populate_parameters
 
+from src.core.database import engine_async, promec_async_engine
+
 
 async def main():
     await create_admin_user()
@@ -13,6 +15,9 @@ async def main():
     await populate_parameter_categories()
     await populate_parameters()
     await populate_fibers()
+
+    await promec_async_engine.dispose()
+    await engine_async.dispose()
 
 
 if __name__ == "__main__":
