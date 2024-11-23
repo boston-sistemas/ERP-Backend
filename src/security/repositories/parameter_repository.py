674e082a5cc7349f9ab2a1e3c5e3ad8b3dc61a39
JohanTv <johan.tanta@utec.edu.pe>
2024-11-22 22:46:06 -0500

    feat(security): add support to retrieve only parameter values in ParameterRepository

--- src/security/repositories/parameter_repository.py
+++ src/security/repositories/parameter_repository.py
@@ -1,5 +1,6 @@
+from sqlalchemy import BinaryExpression
 from sqlalchemy.ext.asyncio import AsyncSession
-from sqlalchemy.orm import joinedload
+from sqlalchemy.orm import joinedload, load_only
 from sqlalchemy.orm.strategy_options import Load
 
 from src.core.repository import BaseRepository
@@ -14,14 +15,46 @@ class ParameterRepository(BaseRepository[Parameter]):
     def include_category() -> Load:
         return joinedload(Parameter.category)
 
-    async def find_parameter_by_id(
-        self, parameter_id: int, include_category=False, **kwargs
-    ) -> Parameter | None:
+    @staticmethod
+    def load_only_value() -> Load:
+        return load_only(Parameter.value, Parameter.is_active, raiseload=True)
+
+    def get_load_options(
+        self, include_category: bool = False, load_only_value: bool = False
+    ) -> list[Load]:
         options: list[Load] = []
 
         if include_category:
             options.append(self.include_category())
 
+        if load_only_value:
+            options.append(self.load_only_value())
+
+        return options
+
+    async def find_parameter_by_id(
+        self,
+        parameter_id: int,
+        include_category: bool = False,
+        load_only_value: bool = False,
+        **kwargs,
+    ) -> Parameter | None:
+        options = self.get_load_options(
+            include_category=include_category, load_only_value=load_only_value
+        )
         parameter = await self.find_by_id(parameter_id, options=options, **kwargs)
 
         return parameter
+
+    async def find_parameters(
+        self,
+        filter: BinaryExpression = None,
+        include_category: bool = False,
+        load_only_value: bool = False,
+    ) -> list[Parameter]:
+        options = self.get_load_options(
+            include_category=include_category, load_only_value=load_only_value
+        )
+        parameters = await self.find_all(filter=filter, options=options)
+
+        return parameters
